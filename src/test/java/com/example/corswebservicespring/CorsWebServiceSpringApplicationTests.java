bf4b06f0b3a5b9ff222e338740ffe2c1615bb567
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-24 08:50:53 +0200

    initial commit

---
+++ src/test/java/com/example/corswebservicespring/CorsWebServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corswebservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsWebServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
